eea6010303dcce4c41b14cdb29e4507b64dc6c15
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-09-24 18:22:41 +0100

    scroll view

--- app/screens/movie_screen.rb
+++ app/screens/movie_screen.rb
@@ -3,19 +3,24 @@ class MovieScreen < PM::Screen
   attr_accessor :movie
 
   def on_load
-    @layout = MovieLayout.new(root: self.view).build
+    @layout = MovieLayout.new
+    self.view = @layout.view
 
     self.title = @movie.title
-
-    @label = @layout.get(:label)
-    @label.text = @movie.title
 
     @poster = @layout.get(:poster)
     @poster.setImageWithURL(@movie.poster.nsurl, placeholderImage: "Default-568h".uiimage)
 
+    @tagline_label = @layout.get(:tagline_label)
+    @tagline_label.text = @movie.tagline
+
+
+    @overview_label = @layout.get(:overview_label)
+    @overview_label.text = @movie.overview
+
+
     @button = @layout.get(:button)
     @button.on(:touch) do
-
       SVProgressHUD.show
       MoviesService.downloadMagnet(@movie.TorrentMagnetUrl) do |error|
         if error
@@ -24,13 +29,7 @@ class MovieScreen < PM::Screen
           SVProgressHUD.dismiss
         end
       end
-
     end
-  end
-
-  def updateViewConstraints
-    @layout.add_constraints(self)
-    super
   end
 
 end
@@ -38,11 +37,19 @@ end
 
 class MovieLayout < MotionKit::Layout
 
+  HorizontalPadding = 10
+  VerticalPadding = 10
+
   def layout
-    root :root do
-      add UILabel, :label
-      add UIImageView, :poster
-      add UIButton, :button
+    root(UIView, :root) do
+      add UIScrollView, :scroll do
+        add UIView, :container do
+          add UIImageView, :poster
+          add UILabel, :tagline_label
+          add UILabel, :overview_label
+          add UIButton, :button
+        end
+      end
     end
   end
 
@@ -50,28 +57,59 @@ class MovieLayout < MotionKit::Layout
     background_color "#F9F9F9".uicolor
   end
 
-  def label_style
-    text 'Movie title'
-    text_color :black.uicolor
-    text_alignment NSTextAlignmentCenter
-
+  def scroll_style
+    background_color "#ff0000".uicolor
     constraints do
-      height.equals(40)
-      width.equals(:superview)
-      left.equals(0)
+      width.equals :root
+      height.equals :root
+      left.equals :root
+      top.equals :container
+      bottom.equals :container
     end
+  end
 
+  def container_style
+    constraints do
+      top.equals(:scroll)
+      left.equals(:scroll)
+      width.equals(:scroll)
+      #height.equals(2000)
+      #bottom.equals(:button)
+    end
   end
 
   def poster_style
+    constraints do
+      top.equals(:superview)
+      height.equals(300)
+
+      left.equals(0)
+      width.equals(:superview)
+    end
+  end
+
+  def tagline_label_style
+    target.lineBreakMode = NSLineBreakByWordWrapping
+    target.numberOfLines = 0
 
     constraints do
-      top.equals(:label, :bottom)
-      left.equals(0)
-      width.equals(:label)
-      height.equals(300)
-    end
+      top.equals(:poster, :bottom).plus(2 * VerticalPadding)
 
+      left.is HorizontalPadding
+      width.equals(:superview).minus(2 * HorizontalPadding)
+    end
+  end
+
+  def overview_label_style
+    target.lineBreakMode = NSLineBreakByWordWrapping
+    target.numberOfLines = 0
+
+    constraints do
+      top.equals(:tagline_label, :bottom).plus(VerticalPadding)
+
+      left.is HorizontalPadding
+      width.equals(:superview).minus(2 * HorizontalPadding)
+    end
   end
 
   def button_style
@@ -80,23 +118,14 @@ class MovieLayout < MotionKit::Layout
     layer do
       corner_radius 7.0
     end
-
     constraints do
-      left.is 8
-      #right.is 8
-      width.equals(:superview).minus(2 * 8)
-      top.equals(:poster, :bottom).plus(20)
-    end
+      top.equals(:overview_label, :bottom).plus(20)
 
-  end
+      left.is HorizontalPadding
+      width.equals(:superview).minus(2 * HorizontalPadding)
 
-  def add_constraints(controller)
-    # guard against adding these constraints more than once
-    unless @layout_constraints_added
-      @layout_constraints_added = true
-      constraints(:label) do
-        below(controller.topLayoutGuide).plus(20)
-      end
+
+      bottom.equals(:superview).minus(VerticalPadding)
     end
   end
 
